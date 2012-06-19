c08782cf1f89d5828e4171775f33bf0188d262c6
Florian Lindner <florian.lindner@xgm.de>
2012-06-19 14:45:54 +0200

    Updated download_flof.sh to copy env_setup to the right place and updated file modes.

--- src/tools/download_flof.sh
+++ src/tools/download_flof.sh
@@ -5,3 +5,8 @@ svn checkout https://github.com/horus107/flof/trunk flof
 
 echo "Checking out PyFoam"
 svn checkout https://openfoam-extend.svn.sourceforge.net/svnroot/openfoam-extend/trunk/Breeder/other/scripting/PyFoam
+
+if [ ! -f env_setup.sh ]; then
+    echo "Copying env_setup.sh to $PWD. Please edit this file and change the BASE variable according to your needs. This usually means the local directory."
+    cp flof/src/tools/env_setup.sh .
+fi
