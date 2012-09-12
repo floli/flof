6502f7c98b0673adf0fe882710698714fb1c9135
Florian Lindner <florian.lindner@xgm.de>
2012-09-12 19:24:44 +0200

    Further work on the CaseCreator worker, added code to create or copy the mesh. Began work to create the boundary conditions. Modified flof.py.

--- src/flof.py
+++ src/flof.py
@@ -7,65 +7,18 @@ logger = logging.getLogger(__name__)
 import common, configuration
 from common import norm_path
 
-from workerstack import WorkerStack
-from workers import Casebuilder
+from workers.baseworker import WorkerFactory, register_bundled_workers
 
+from workers.foamutility import ChangeDictionary
 
-class ControlServer():
-    """ XML-RPC Server that is used to get information about the actions flof is performing.
-    All methods are exposed to the RPC interface. """
+import xml.etree.ElementTree as ET
 
-    returncode = 0
-
-    def __init__(self, config, worker_stack, auto_run = False):
-        self.w_stack = worker_stack
-        
-        port = config.getint("general", "slave_port")
-        self.server = common.ThreadedXMLRPCServer( ('', port), allow_none = True, logRequests=False )
-        self.server.register_introspection_functions()
-        self.server.register_instance(self)
-        self.server.daemon_threads = True # Threads are killed when shutdown initiated.
-        logger.info("flof listening on port %i.", port)
-        if auto_run:
-            t = threading.Thread(target=self.run)
-            t.daemon = True
-            t.start()
-        self.server.serve_forever()
-
-    def run(self):
-        """ Starts the job. The function itself is blocking, but the server is threaded.
-        Shutdown after execution. """
-
-        try:
-            self.w_stack.execute()
-        except: # Since run() is a seperate thread any exception not caught here will be lost.
-            traceback.print_exc()
-        finally: # Make sure the server is always shut down.
-            self.server.shutdown()
-        
-    def abort(self):
-        """ Aborts the job, quits the programm. """
-        self.returncode = common.ST_ABORTED
-        logger.info("Got abort signal, initiating shut down.")
-        self.w_stack.abort()
-        print "Returned from w_stack.abort"
-                
-
-    def worker_info(self):
-        """ Returns a info dict that is custom to the worker, e.g. timestep. """
-        return self.w_stack.worker_info()
-
-    def active_worker(self):
-        """ Returns the worker that is currently running. """
-        return self.w_stack.active_worker()
-        
 
 def add_options():
     """ Factory function for the OptionParser. """
     parser = optparse.OptionParser(usage="%prog [options] config_file")
     parser.add_option("-f", "--file", help="Save the pyFoamCaseBuilder file to the given location and exit.")
     parser.add_option("-c", "--config", help="Add/overwrite configuration options. Syntax: 'section.key=value,...'")
-    parser.add_option("-n", "--no-run", help="Do not run the case. The run must be initiated through the XML-RPC method run().", action="store_true", dest="no_run")
     return parser
 
 
@@ -81,21 +34,18 @@ def main():
     if not os.path.isfile(args[0]):
         print "Configuration file %s not existing." % args[0]
         sys.exit(-1)
-        
-    config = configuration.Configuration(args[0], options.config)
+
+
+    register_bundled_workers()
+    
+    config = ET.parse(args[0])
     os.chdir(os.path.dirname(norm_path(args[0])))
 
-    w_stack = WorkerStack(config)
-    
-    if options.file:
-        f = open(norm_path(options.file), "w")
-        cbw = w_stack.get_worker_by_type(Casebuilder)
-        cbw.create_pfcb(f, cbw.config["case_template"])
-        sys.exit()
-    
-    server = ControlServer(config, w_stack, not options.no_run)
+    wf = WorkerFactory(config)
 
-    sys.exit(server.returncode)
+    for w in wf.workers():
+        if w.do:
+            w.run()
     
 
 if __name__ == "__main__":
--- src/workers/casecreator.py
+++ src/workers/casecreator.py
@@ -1,8 +1,16 @@
 import os, re, shutil
 from os.path import join
 
+from PyFoam.RunDictionary.ParsedParameterFile import ParsedBoundaryDict, WriteParameterFile
+
 from baseworker import BaseWorker
 from common import norm_path
+
+
+_OF_dimensions = {
+    "U" : "[0 1 -1 0 0 0 0]",
+    "p" : "[0 2 -2 0 0 0 0]"
+    }
 
 
 class CaseCreator(BaseWorker):
@@ -14,11 +22,11 @@ class CaseCreator(BaseWorker):
             if os.path.isfile(join(src_dir, f)):
                 for tag in dir_node.findall("./file"):
                     if re.match(tag.attrib["name"], f):
-                        print "COPY FROM %s TO %s"  % (join(src_dir, f), join(target,f ))
                         try:
                             os.makedirs(target)
                         except OSError:
                             pass # Directory already exists
+                        self.logger.debug("Copy file from  %s to %s"  % (join(src_dir, f), join(target,f )))
                         shutil.copy( join(src_dir, f), join(target, f) )
             elif os.path.isdir(join(src_dir, f)):
                 for tag in dir_node.findall("./directory"):
@@ -33,5 +41,39 @@ class CaseCreator(BaseWorker):
         self._copy_rec("", files_tag)
 
 
+    def create_mesh(self):
+        if self.config.find("./mesh/fluent") is not None:
+            tag = self.config.find("./mesh/fluent")
+            mesh = norm_path(tag.attrib["mesh"])
+            cmd = "fluentMeshToFoam -case %s %s %s" % (self.case, tag.get("arguments", ""), mesh)
+            self.logger.info("Creating mesh from fluent mesh file %s", mesh)
+            self.start_process(cmd)
+        elif self.config.find("./mesh/copy") is not None:
+            tag = self.config.find("./mesh/copy")
+            src_case = norm_path(tag.get("source", self.config.attrib["template"]))
+            time = tag.get("time", "constant")
+            src_path = join(src_case, time, "polyMesh")
+            self.logger.info("Copy mesh from %s", src_path)
+            shutil.copytree(src_path, join(self.case, "constant/polyMesh"))
+
+
+    def create_BCs(self):
+        in_BCs = []
+        for bc in self.config.findall("./boundaries/boundary"):
+            in_BCs.append( (bc.attrib["name"], bc.attrib["pattern"]) )
+
+        mesh_BCs = ParsedBoundaryDict(join(self.case, "constant/polyMesh/boundary"))
+        os.mkdir(join(self.case, "0"))
+        import pdb; pdb.set_trace() 
+        for field in self.config.findall("./fields/field"):
+            field_name = field.attrib["name"]
+            field_file = WriteParameterFile(join(self.case, "0", field_name))
+            field_file.content["dimensions"] = _OF_dimensions[field_name]
+            field_file.content["internalField"] = field.find("./ic").attrib["value"]
+            field_file.writeFile()
+            
+
     def run(self):
         self.copy_files()
+        self.create_mesh()
+        self.create_BCs()
